e8ac53b16445ed6907122d7c36134d1455f2cf33
rhw <ruohanw89@gmail.com>
2020-06-20 14:12:27 -0700

    make them sortable

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,5 +143,47 @@ $("#remove-tasks").on("click", function () {
   saveTasks();
 });
 
+// sort table
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  // activate: function (event) {
+  //   console.log("activate", this);
+  // },
+  // deactivate: function (event) {
+  //   console.log("deactivate", this);
+  // },
+  // over: function (event) {
+  //   console.log("over", this);
+  // },
+  // out: function (event) {
+  //   console.log("out", event.target);
+  // },
+  update: function (event) {
+    // create a new temporary array
+    var tempArr = [];
+    //loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function () {
+        var text = $(this).find("p").text().trim();
+        var date = $(this).find("span").text().trim();
+        // add task data to the temp array as an object
+        tempArr.push({
+          text: text,
+          data: date,
+        });
+      });
+    // trim down list' ID to match object property
+    var arrName = $(this).attr("id").replace("list-", "");
+    // update arry on tasks object and save tasks [arrName]= temArr;
+    tasks[arrName] = tempArr;
+    console.log(tasks);
+    saveTasks();
+  },
+});
+
 // load tasks for the first time
 loadTasks();
